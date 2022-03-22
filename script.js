c748cdf1f9aa253c1a6b2b4c4aaa318c6276b79e
Cauã dantas <cauadsouza@gmail.com>
2022-03-22 12:13:12 -0300

    update geral

--- script.js
+++ script.js
@@ -1,47 +1,115 @@
-class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
+/*mundos*/
+var unicadiv = window.document.getElementById('div_ex')
 
-        this.handleClick = this.handleClick.bind(this);
-    }
-  
-    animateLinks() {
-        this.navLinks.forEach((link, index) => {
-        link.style.animation
-            ? (link.style.animation = "")
-            : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-                index / 7 + 0.3
-            }s`);
-        });
-    }
-  
-    handleClick() {
-        this.navList.classList.toggle(this.activeClass);
-        this.mobileMenu.classList.toggle(this.activeClass);
-        this.animateLinks();
-    }
-  
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-  
-    init() {
-        if (this.mobileMenu) {
-        this.addClickEvent();
-        }
-        return this;
-    }
-  }
-  
-  const mobileNavbar = new MobileNavbar(
-        ".menu_lista",
-        ".menu_lista",
-        ".menu_lista li",
-  );
-  mobileNavbar.init();
+var unicadiv2 = window.document.getElementById('div_ex2')
 
+var unicadiv3 = window.document.getElementById('div_ex3')
 
-/*Resenhas*/
+var unicadiv4 = window.document.getElementById('div_ex4')
+
+var unicadiv5 = window.document.getElementById('div_ex5')
+
+var unicadiv6 = window.document.getElementById('div_ex6')
+
+var unicadiv7 = window.document.getElementById('div_ex7')
+
+var unicadiv8 = window.document.getElementById('div_ex8')
+
+var unicadiv9 = window.document.getElementById('div_ex9')
+
+var unicadiv10 = window.document.getElementById('div_ex10')
+
+var unicadiv11 = window.document.getElementById('div_ex11')
+
+var unicadiv12 = window.document.getElementById('div_ex12')
+
+var unicadiv13 = window.document.getElementById('div_ex13')
+
+var unicadiv14 = window.document.getElementById('div_ex14')
+
+var unicadiv15 = window.document.getElementById('div_ex15')
+
+var unicadiv16 = window.document.getElementById('div_ex16')
+
+function exibir(){
+    unicadiv.style.display = "flex"
+}
+
+function exibir2(){
+    unicadiv2.style.display = "flex"
+}
+
+function exibir3(){
+    unicadiv3.style.display = "flex"
+}
+
+function exibir4(){
+    unicadiv4.style.display = "flex"
+}
+
+function exibir5(){
+    unicadiv5.style.display = "flex"
+}
+
+function exibir6(){
+    unicadiv6.style.display = "flex"
+}
+
+function exibir7(){
+    unicadiv7.style.display = "flex"
+}
+
+function exibir8(){
+    unicadiv8.style.display = "flex"
+}
+
+function exibir9(){
+    unicadiv9.style.display = "flex"
+}
+
+function exibir10(){
+    unicadiv10.style.display = "flex"
+}
+
+function exibir11(){
+    unicadiv11.style.display = "flex"
+}
+
+function exibir12(){
+    unicadiv12.style.display = "flex"
+}
+
+function exibir13(){
+    unicadiv13.style.display = "flex"
+}
+
+function exibir14(){
+    unicadiv14.style.display = "flex"
+}
+
+function exibir15(){
+    unicadiv15.style.display = "flex"
+}
+
+function exibir16(){
+    unicadiv16.style.display = "flex"
+}
+
+function fechar(){
+    unicadiv.style.display = "none"
+    unicadiv2.style.display = "none"
+    unicadiv3.style.display = "none"
+    unicadiv4.style.display = "none"
+    unicadiv5.style.display = "none"
+    unicadiv6.style.display = "none"
+    unicadiv7.style.display = "none"
+    unicadiv8.style.display = "none"
+    unicadiv9.style.display = "none"
+    unicadiv10.style.display = "none"
+    unicadiv11.style.display = "none"
+    unicadiv12.style.display = "none"
+    unicadiv13.style.display = "none"
+    unicadiv14.style.display = "none"
+    unicadiv15.style.display = "none"
+    unicadiv16.style.display = "none"
+}
